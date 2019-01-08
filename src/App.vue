704685441a77f8cff207ced1fd2dc3270fdf16cb
Freddie Levenson <freddielev@gmail.com>
2019-01-07 20:35:30 -0700

    Add images, add buttons, Change image SRC on click

--- src/App.vue
+++ src/App.vue
@@ -19,10 +19,25 @@ export default {
 </script>
 
 <style>
-    .heading-holder {
+@import url('https://fonts.googleapis.com/css?family=Berkshire+Swash');
+
+  .heading-holder {
     display: flex;
     align-items: center;
     justify-content: center;
     width: 100vw;
+  }
+
+  .heading {
+    font-size: 4rem;
+    margin-bottom: 20px;
+    font-family: 'Berkshire Swash', cursive;
+    color: white;
+  }
+  
+  #app {
+    background-color: black;
+    width: 100vw;
+    height: 100vh;
   }
 </style>
--- src/components/ImageContainer.vue
+++ src/components/ImageContainer.vue
@@ -1,25 +1,79 @@
 <template>
-  <div>
+  <div class='main-container'>
+    <div class='image-holder'>
+      <img class='main-image' :src='mainSRC' alt='Nasa Image of the day'/>
+    </div>
+    <div class='button-holder'>
+      <ul>
+        <li v-for="image in images" v-bind:key='image.id' v-bind:id='image.id'><button v-on:click='setImage(image.src)'>View</button></li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+
+import image from '../assets/jupiter.jpg'
+import image2 from '../assets/space_1.jpg'
+import image3 from '../assets/space_2.jpeg'
+import image4 from '../assets/space_cat.jpeg'
+
+
 export default {
   name: 'ImageContainer',
+  mainImage: this.image || image,
   data () {
     return {
-      image: null
+      image: null,
+      mainSRC: image,
+      images: [
+        {
+          src: image,
+          index: 0
+        },
+        {
+          src: image2,
+          index: 1,
+        }, 
+        {
+          src: image3,
+          index: 2
+        },
+        {
+          src: image4,
+          index:3
+        }
+        ]
     }
   },
   methods: {
-    loadImage: async function() {
-      
+    loadImage: function() {
+      return '../assets/jupiter.jpg'
+    },
+    setImage: function(tag) {
+      this.mainSRC = tag
     }
-  }
+  },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+  .main-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    height: 75vh;
+  }
+  .main-image {
+    width: 500px;
+  }
+  
+  .image-holder{
+    width: 100vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 </style>
