d7fa756b0758d9755378a28afce8c86b8b070ccf
Freddie Levenson <freddielev@gmail.com>
2019-01-07 18:44:43 -0700

    Initial Commit, initial vue structure

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <div class="heading-holder">
+      <span class="heading">SpaceYourself</span>
+    </div>
+    <ImageContainer/>
+  </div>
+</template>
+
+<script>
+import ImageContainer from './components/ImageContainer'
+
+export default {
+  name: 'App',
+  components: {
+    ImageContainer
+  }
+}
+</script>
+
+<style>
+    .heading-holder {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+  }
+</style>
---
+++ src/components/ImageContainer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImageContainer',
+  data () {
+    return {
+      image: null
+    }
+  },
+  methods: {
+    loadImage: async function() {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
